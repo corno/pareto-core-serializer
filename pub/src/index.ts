dfcb811c2e4382e37967ba425c505b0866a3f3f9
Corno Schraverus <corno@schraverus.com>
2026-01-03 17:24:10 +0100

    .

--- pub/src/index.ts
+++ pub/src/index.ts
@@ -4,19 +4,24 @@ import * as _pinternals from "pareto-core-internals"
  * these functions coming from core-internals should be exposed for serializer development
  */
 export {
+    
     unreachable_code_path,
-    integer_divide,
 
     cc,
     au,
     ss,
+
 } from "pareto-core-internals"
+
+export namespace integer {
+
+    export const divide = _pinternals.integer_divide
+
+}
 
 export namespace natural {
 
-    export const text_length = (text: string): number => {
-        return text.length
-    }
+    export const text_length = (text: string): number => text.length
 
 }
 
