8c013b753d77019b880802b06cd476a66640830f
Corno Schraverus <corno@schraverus.com>
2026-01-03 16:34:36 +0100

    .

--- pub/src/index.ts
+++ pub/src/index.ts
@@ -1,17 +1,33 @@
+import * as _pinternals from "pareto-core-internals"
+
 /**
  * these functions coming from core-internals should be exposed for serializer development
  */
 export {
-    text_build,
-    list_build,
-    panic as unreachable_code_path,
+    unreachable_code_path,
     integer_divide,
-    
+
     cc,
     au,
     ss,
 } from "pareto-core-internals"
 
-export const get_text_length = (text: string): number => {
-    return text.length
+export namespace natural {
+
+    export const text_length = (text: string): number => {
+        return text.length
+    }
+
+}
+
+export namespace list {
+
+    export const build = _pinternals.list_build
+
+}
+
+export namespace text {
+
+    export const build = _pinternals.text_build
+
 }
